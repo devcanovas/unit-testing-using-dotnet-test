1971c56c810264f746e7e55fa995ff7173fa6c86
matheus <matheuscanovas9@gmail.com>
2024-02-29 12:32:32 -0300

    uploading the project

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,10 @@
+﻿namespace Prime.Service;
+
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2) return false;
+        throw new NotImplementedException("Not implemented.");
+    }
+}
